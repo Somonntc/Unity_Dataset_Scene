e7a86b6388ceae3ebc81bfd70b3195bae2dc798d
Somon <alexandre.somon@isen.yncrea.fr>
2020-03-23 13:59:27 +0100

    Record scene with script

---
+++ Assets/Scripts/Record.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Recorder;
+using UnityEditor;
+
+public class Record : MonoBehaviour
+{
+    private RecorderWindow recorderWindow;
+    [SerializeField] private GameObject prefabPosition;
+    [SerializeField] private GameObject prefab;
+    private GameObject model;
+    private Transform cameraTransform;
+    private Transform start;
+
+    private void Awake() {
+        cameraTransform = prefabPosition.GetComponent<Transform>();
+        Debug.Log("cameraTransform : "+ cameraTransform.position);
+    }
+    private static RecorderWindow GetRecorderWindow(){
+        return (RecorderWindow)EditorWindow.GetWindow(typeof(RecorderWindow));
+    }
+
+    private void Start() {
+        //StartCoroutine(StartStopRecord());
+        StartCoroutine(test());
+    }
+
+    private IEnumerator test(){
+        while(true){
+            model = Instantiate(prefab, new Vector3(0f , 0f, 0f), Quaternion.Euler(0f, 90f, 0f));
+            Destroy(model, 3f);
+            cameraTransform.Rotate(prefabPosition.transform.rotation.x, prefabPosition.transform.rotation.y + 90, prefabPosition.transform.rotation.z);
+            Debug.Log("CameraTransform in coroutine : " + cameraTransform.position);
+            RecorderWindow recorderWindow = GetRecorderWindow();
+            if(!recorderWindow.IsRecording()){
+                Debug.Log("Start recording");
+                recorderWindow.StartRecording();
+                Debug.Log("Is recording (Start boucle): " + recorderWindow.IsRecording());
+            }else{
+                Debug.Log("is Recording : (stop and start boucle) " + recorderWindow.IsRecording());
+                Debug.Log("Stop and Start recording");
+                recorderWindow.StopRecording();
+                Debug.Log("is Recording : (after stop) " + recorderWindow.IsRecording());
+                Debug.Log("Start again recording");
+                recorderWindow.StartRecording();
+                Debug.Log("is Recording : (after start) " + recorderWindow.IsRecording());
+                Debug.Log("End of boucle");
+            }
+            yield return new WaitForSeconds(3f);
+        }
+    }
+    
+}
